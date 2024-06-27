2bf4515107de008bc4bcbb78b321280988561713
Frans <frans.trujillo@utec.edu.pe>
2024-06-26 19:45:24 -0500

    ultimos cambios

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,7 @@ import cors from 'cors';
 import createTable from './database-migration.js';
 
 const app = express();
-const port = 3001;
+const port = 443;
 
 //middlewares
 app.use(express.json());
@@ -23,85 +23,6 @@ app.get('*', (req, res) => {
 });
 
 //rutas
-
-/*app.get('/connected', async (req, res) => {
-  try {
-    const client = await pool.connect();
-    const result = await client.query('SELECT NOW() as current_time');
-    client.release(); // Liberar el cliente de la conexión de vuelta al pool
-
-    res.send(`Current time is: ${result.rows[0].current_time}`);
-  } catch (error) {
-    console.error('Error connecting to the database:', error);
-    res.status(500).send('Internal Server Error');
-  }
-});
-
-// Obtener todos los usuarios
-app.get('/usuarios',checkUserType("laprincesa"), async (req, res) => {
-  try {
-    const client = await pool.connect();
-    const result = await client.query('SELECT * FROM usuarios');
-    client.release();
-    res.json(result.rows);
-  } catch (error) {
-    console.error('Error en GET /usuarios:', error);
-    res.status(500).json({ error: 'Error interno del servidor' });
-  }
-});
-
-app.post('/usuarios', async (req, res) => {
-  const { nombre, email, edad, tipo } = req.body;
-  try {
-    const client = await pool.connect();
-    const result = await client.query('INSERT INTO usuarios (nombre, email, edad,tipo) VALUES ($1, $2, $3, $4) RETURNING *', [nombre, email, edad, tipo]);
-    client.release();
-    res.status(201).json(result.rows[0]);
-  } catch (error) {
-    console.error('Error en POST /usuarios:', error);
-    res.status(500).json({ error: 'Error interno del servidor' });
-  }
-});
-
-app.put('/usuarios/:id', async (req, res) => {
-  const id = req.params.id;
-  const { nombre, email, edad, tipo} = req.body;
-  try {
-    const client = await pool.connect();
-    const result = await client.query('UPDATE usuarios SET nombre = $1, email = $2, edad = $3, tipo = $4 WHERE id = $5 RETURNING *', [nombre, email, edad, tipo, id]);
-    client.release();
-    if (result.rows.length === 0) {
-      res.status(404).json({ error: 'Usuario no encontrado' });
-    } else {
-      res.json(result.rows[0]);
-    }
-  } catch (error) {
-    console.error('Error en PUT /usuarios/:id:', error);
-    res.status(500).json({ error: 'Error interno del servidor' });
-  }
-});
-
-app.delete('/usuarios/:id', async (req, res) => {
-  const id = req.params.id;
-  try {
-    const client = await pool.connect();
-    const result = await client.query('DELETE FROM usuarios WHERE id = $1 RETURNING *', [id]);
-    client.release();
-    if (result.rows.length === 0) {
-      res.status(404).json({ error: 'Usuario no encontrado' });
-    } else {
-      res.json({ mensaje: 'Usuario eliminado correctamente' });
-    }
-  } catch (error) {
-    console.error('Error en DELETE /usuarios/:id:', error);
-    res.status(500).json({ error: 'Error interno del servidor' });
-  }
-});
-
-app.get('/', (req, res) => {
-  res.send('hola, estás en el root')
-});
-*/
 
 
 app.get('/messages', async (req, res) => {
